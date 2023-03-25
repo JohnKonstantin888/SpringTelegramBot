7bef3196c701781fc8d578167eb33b40e7226e2d
Konstantin Kovalyov <Konstantin-kovalyo@yandex.ru>
2023-03-25 18:30:03 +0300

    Telegram Bot v1

---
+++ src/main/java/com/example/SpringTelegramBot/service/TelegramBot.java
@@ -0,0 +1,58 @@
+package com.example.SpringTelegramBot.service;
+
+import com.example.SpringTelegramBot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    private final BotConfig botConfig;
+
+    public TelegramBot(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (message) {
+                case "/start" -> startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                default -> sendMessage(chatId, "Sorry, this command was not recognized");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Hi, " + name + ", nice to meet you!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(textToSend);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+}
