eef527b14c949e634a1120d90f02f526fad17272
Konstantin Kovalyov <Konstantin-kovalyo@yandex.ru>
2023-03-26 12:54:51 +0300

    add connection db and dao v4

---
+++ src/main/java/com/example/SpringTelegramBot/model/User.java
@@ -0,0 +1,22 @@
+package com.example.SpringTelegramBot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Entity(name = "usersDataTable")
+public class User {
+    @Id
+    private Long chatId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+
+    private Timestamp registeredAt;
+}
