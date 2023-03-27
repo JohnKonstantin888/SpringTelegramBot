9c781412044e75e0bb15f248c6ae06b2c6da768c
Konstantin Kovalyov <Konstantin-kovalyo@yandex.ru>
2023-03-27 19:44:16 +0300

    refactor code, add reference v6

--- src/main/java/com/example/SpringTelegramBot/model/repository/UserRepository.java
+++ src/main/java/com/example/SpringTelegramBot/model/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.example.SpringTelegramBot.model;
+package com.example.SpringTelegramBot.model.repository;
 
+import com.example.SpringTelegramBot.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
